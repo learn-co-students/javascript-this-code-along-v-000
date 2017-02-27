6dce574cce0723e157ebd0ddfa0b9abe26856da9
Rebecca <beckysherman000@gmail.com>
2017-02-26 21:28:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+pbj.describe();
+pbj.describe.call(salad);
+
+salad.describe = pbj.describe.bind(salad);
+salad.describe()
+
+// function serve(customer) {
+//   console.log("Hey " + customer + ", here's your " + this.name + ", enjoy!");
+// }
+
+function serve() {
+  if(arguments.length > 0) {
+      var customers = Array.prototype.slice.call(arguments);
+      last = customers.pop();
+      console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+  }else {
+      console.log(this.name + ". Order up!");
+  }
+}
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
+
+serve.apply(gc, ["Terry"]);
+serve.apply(pbj, ["Jesse"]);
+
+function deliverFood(customer, table) {
+  console.log("Delivering " + this.name + " to " + customer + " at table " + table);
+}
+
+deliverFood.call(gc, "Terry", "4");
+deliverFood.apply(pbj, ["Jesse", "15"]);
+
+serve.call(gc);
+serve.apply(pbj, ["Terry", "Tom", "Tabitha"]);
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+//create new Customer instance
+var sally = new Customer("Sally", "4");
+
+//schedule table visit
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
