4f0162db52fd702a77008c7df5c2495495844fb6
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-03-25 00:19:42 -0700

    Done.

--- index.js
+++ index.js
@@ -136,3 +136,38 @@ pbj.describe.call(salad);
 //using bind()
 var describeSalad = pbj.describe.bind(salad);
 describeSalad();
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+salad.describe = pbj.describe.bind(salad);
+
+//async execution with bind()
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+//create new Customer instance
+var sally = new Customer("Sally", "4");
+
+//schedule table visit
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
