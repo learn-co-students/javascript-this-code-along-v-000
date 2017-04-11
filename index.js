d38b585e30f88612e3ef73071c72a91144ff288b
Ivan Pilot <ivanpilot@gmail.com>
2017-04-11 22:44:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+function visitTable(){
+  console.log("The server is visiting " + this.name + " at table " + this.tableNumber + ".");
+}
+
+function Customer(name, tableNumber){
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+var sally = new Customer("Sally", "4");
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
