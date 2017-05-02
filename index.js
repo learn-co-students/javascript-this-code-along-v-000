716973575c83b4b9232d53c34743ffcbd1ee6437
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-02 08:56:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+function Sandwich(bread, ingredients, cut) {
+    this.bread = bread;
+    this.ingredients = ingredients;
+    this.cut = cut;
+    
+}
+
+function serve(customer) {
+        console.log("Hey, " + customer + ", here's your " + this.name + ", enjoy!")
+    }
+var blt = new Sandwich("white",["bacon","lettuce","tomato","mayo"],"rectangle");
+blt.name = "BLT";
+serve.call(blt, "michael");
