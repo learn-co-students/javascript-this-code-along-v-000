4f3d95f5d2f49918aa959c62ea16701dd9edb843
Callie Brazil <cmabrazil@gmail.com>
2017-05-12 22:19:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles"
+}
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+}
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+var reuben = new Sandwich("rye", ["corned beef","sauerkraut","swiss","russian dressing"],"diagonal");
+
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles",
+  name: "peanut butter and jelly",
+  serve: function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
