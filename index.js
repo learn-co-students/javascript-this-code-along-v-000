53e77cd6f278217932495e46fb287d2928e5505c
Seth <itz.saga@gmail.com>
2017-05-29 12:22:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+
+function serve() {
+  console.log("here's your " + this.name + ", enjoy!");
+}
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
