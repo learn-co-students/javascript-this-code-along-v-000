b73238f093cc8ce3ff01973831dd44d069555800
Isis Tejeda <isis@isistejeda.com>
2017-06-21 21:19:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+pbj.describe();
+
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+pbj.describe.call(salad);
