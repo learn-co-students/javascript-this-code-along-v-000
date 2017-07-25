d872c945347233db7aea3b9438a8e9e8920714e1
github username apalski <github email address annette.drapalski@gmail.com>
2017-07-25 15:41:49 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function serve() {
+    if(arguments.length > 0) {
+        var customers = Array.prototype.slice.call(arguments);
+        last = customers.pop();
+        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+    }else {
+        console.log(this.name + ". Order up!");
+    }
+}
