064a755069517a3962bed56c296a832ce8f6f376
r0tiart <antley.chu@gmail.com>
2017-08-10 13:29:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.serve = function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+ 
+function serve(customer) {
+  console.log("Hey " + customer + ", here's your " + this.name + ", enjoy!");
+}
+ 
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"],
+"Peanut Butter & Jelly");
