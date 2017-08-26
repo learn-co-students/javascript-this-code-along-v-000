bebdd476e51b19203315dc21a590097eda5c1a4d
James Fernandez <jffernan@comcast.net>
2017-08-26 23:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-const app = "I don't do much."
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles"
+}
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+}
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+var reuben = new Sandwich("rye", ["corned beef","sauerkraut","swiss","russian dressing"],"diagonal");
+
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles",
+  name: "peanut butter and jelly",
+  serve: function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+pbj.serve();
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+// add name to the blt before serving
+blt.name = "BLT";
+blt.serve();
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+
+function serve() {
+  console.log("here's your " + this.name + ", enjoy!");
+}
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
+
+serve.call(gc);
+serve.call(pbj);
