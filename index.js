c850d25d3ebbee655a7f6f120be916c02178211f
Mollie <mollie.stein@gmail.com>
2017-09-03 17:37:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+
+blt.serve();
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
