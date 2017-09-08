d1b315ebb99edff6f840d6dd32b50a7269e69978
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-08 12:51:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+salad.describe = pbj.describe.bind(salad);
+
+function serve(){
+  console.log("here's your " + this.name + ", enjoy!")
+}
+
+var blt = new Sandwich("white", ["bacon", "lettuce", "tomato", "mayo"], "rectangle")
+
+var reuben = new Sandwich("rye", ["corned beef","sauerkraut","swiss","russian dressing"],"diagonal");
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+var sally = new Customer("Sally", "4");
+
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
