526e16a2793afecfec5649a857c3efa922cb8e10
Alexandra Wright <superbiscuit@gmail.com>
2017-12-04 09:50:00 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+
+var blt = {
+    bread: "white",
+    ingredients: ["bacon", "lettuce", "tomato"],
+    cut: "diagonal",
+    name: "BLT",
+    serve: function () {
+        console.log("here's your " + this.name + ", enjoy!");
+    }
+}
+
+blt.serve();
+
+function Sandwich(bread, ingredients, cut) {
+    this.bread = bread;
+    this.ingredients = ingredients;
+    this.cut = cut;
+    this.serve = function () {
+        console.log("here's your " + this.name + ", enjoy!");
+    }
+}
+
+var blt = new Sandwich("white", ["bacon", "lettuce", "tomato", "mayo"], "rectangle");
+
+blt.serve();
+
+function Sandwich(bread, ingredients, cut) {
+    this.bread = bread;
+    this.ingredients = ingredients;
+    this.cut = cut;
+    this.serve = function () {
+        console.log("here's your " + this.name + ", enjoy!");
+    }
+}
+
+var blt = new Sandwich("white", ["bacon", "lettuce", "tomato", "mayo"], "rectangle");
+// add name to the blt before serving
+blt.name = "BLT";
+blt.serve();
